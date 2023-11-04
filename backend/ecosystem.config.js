53667c8eea304d3c6bd8e136745e946cd29f244d
bgonzalez <bgonzalez@hcdn.gob.ar>
2023-11-04 10:42:16 -0300

    PM2

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	apps: [{
+		name: "Arquitectura Web",
+		namespace: "arquitectura-web",
+		script: "./dist/main.js",
+		error_file: './logs/err.log',
+		out_file: './logs/out.log',
+		time: true,
+	}]
+};
